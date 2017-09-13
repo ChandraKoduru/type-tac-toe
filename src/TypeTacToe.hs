e98e0e12b37c1b62c3c3c3b0aaecd4c7f4616108
Chandra Koduru <kscraja@gmail.com>
2017-09-13 12:14:08 +0530

    playing with check that alternate turns game boards with valid(game1) and invalid states(game3, game4)

--- src/TypeTacToe.hs
+++ src/TypeTacToe.hs
@@ -7,6 +7,8 @@
 
 module TypeTacToe where
 
+import Data.Function ((&))
+
 -- | Either X, O or Nothing
 data PieceT = X | O | N
   deriving (Show, Eq)
@@ -54,12 +56,28 @@ type family Played (x :: CoordT) (y :: CoordT) (b :: BoardRep) :: Bool where
   Played x y ('Cons x y _ _) = 'True
   Played x y ('Cons _ _ _ rest) = Played x y rest
 
-playX :: (Played x y b ~ 'False) => (Coord x, Coord y) -> Board b PieceT -> Board ('Cons x y 'X b) PieceT
+-- | whose turn is it now ?
+type family Turn (b :: BoardRep) :: PieceT where
+  Turn ('Cons _ _ 'X _) = 'O
+  Turn _ = 'X
+
+-- | playX only when the cell is not occupied and is x's turn
+playX :: (Played x y b ~ 'False, Turn b ~ 'X) => (Coord x, Coord y) -> Board b PieceT -> Board ('Cons x y 'X b) PieceT
 playX (coordVal -> x, coordVal -> y) (Board b) 
   = Board $ overTrip y (overTrip x (const X)) b
 
-playO :: (Played x y b ~ 'False) => (Coord x, Coord y) -> Board b PieceT -> Board ('Cons x y 'O b) PieceT
+-- | playX only when the cell is not occupied and is y's turn
+playO :: (Played x y b ~ 'False, Turn b ~ 'O) => (Coord x, Coord y) -> Board b PieceT -> Board ('Cons x y 'O b) PieceT
 playO (coordVal -> x, coordVal -> y) (Board b)
   = Board $ overTrip y (overTrip x (const O)) b
 
+game1 = newBoard & playX (A', B')
+game2 = newBoard & playX (A', B') & playO (A', C')
 
+-- | invalid moves
+
+-- wrong turn. When un-commented, the module will not compile
+-- game3 = newBoard & playX (A', B') & playX (A', C')
+
+-- playing into an occupied cell. When un-commented, the module will not compile
+-- game4 = newBoard & playX (A', B') & playO (A', B')
